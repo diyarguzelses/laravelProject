ea527de99ab10cca43b89b35085f4bd9c74d4ed1
beyzaytmr <101430436+beyzaytmr@users.noreply.github.com>
2023-07-09 04:38:35 +0300

    Kullanıcı routeları ve controllerları oluştutuldu

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,14 @@ use Illuminate\Support\Facades\Route;
 //anasayfa
 Route::get('/','App\Http\Controllers\AnasayfaController@index')->name('anasayfa');
 
+Route::group(['prefix'=>'kullanici'],function(){
+    //OturumAc
+    Route::get('/oturumac','App\Http\Controllers\KullaniciController@giris_form')->name('kullanici.oturumac');
+    Route::post('/oturumac','App\Http\Controllers\KullaniciController@giris');
+    //Kaydol
+    Route::get('/kaydol','App\Http\Controllers\KullaniciController@kaydol_form')->name('kullanici.kaydol');
+    Route::post('/kaydol','App\Http\Controllers\KullaniciController@kaydol');
+    //çıkışyap
+    Route::post('/oturumukapat','App\Http\Controllers\KullaniciController@oturumukapat')->name('kullanici.oturumukapat');
+});
 
